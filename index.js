2dbe130266c5d03bba990e3e6ce1b1278e3a851a
Otto <95004246+Otorb@users.noreply.github.com>
2022-08-16 22:01:11 -0500

    Update index.js
    cambio

--- index.js
+++ index.js
@@ -20,11 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const temperamentApi = require('./src/routes/Temperament')
-//const {PORT} = process.env;
+const {PORT} = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
 
     temperamentApi()
